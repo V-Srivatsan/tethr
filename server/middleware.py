9c3e37884e8320b8ab40a20c849075a43ec5545b
V-Srivatsan <vatsan.programming@gmail.com>
2026-05-14 18:38:58 +0530

    Made auth header optional

--- server/middleware.py
+++ server/middleware.py
@@ -2,7 +2,7 @@ from fastapi import Header, HTTPException
 import jwt
 import os
 
-async def get_user(authorization: str | None = Header()):
+async def get_user(authorization: str | None = Header(default=None)):
     if not authorization:
         raise HTTPException(status_code=401, detail="Authorization header is missing")
     
