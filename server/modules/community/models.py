ea554f24630dc6b1d342e1d5be59a011379646fe
V-Srivatsan <vatsan.programming@gmail.com>
2026-05-14 18:41:04 +0530

    Updated reverse relation name

--- server/modules/community/models.py
+++ server/modules/community/models.py
@@ -10,7 +10,7 @@ class Community(BaseModel):
 
 class Membership(BaseModel, BaseTimeMixin):
     user = fields.ForeignKeyField("user.User", related_name="memberships")
-    community = fields.ForeignKeyField("community.Community", related_name="memberships")
+    community = fields.ForeignKeyField("community.Community", related_name="members")
     is_admin = fields.BooleanField(default=False)
     is_active = fields.BooleanField(default=True)
     verified = fields.BooleanField(default=False)
