89379806924b1eec87973ca5669af5e463c58ce4
V-Srivatsan <vatsan.programming@gmail.com>
2026-05-17 09:24:05 +0530

    Client setup

---
+++ tethr/android/app/src/main/kotlin/com/codex_invictus/tethr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codex_invictus.tethr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ tethr/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Tethr',
+    home: Scaffold(
+      body: Center(
+        child: Text("Welcome to Tethr"),
+      )
+    )
+  ));
+}
